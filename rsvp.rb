b00e9134a137df570e6cf22151d9becd2345ad8c
C. Flores <flores@petalphile.com>
2012-03-01 22:38:51 -0800

    more errors, confirm cancellations

--- rsvp.rb
+++ rsvp.rb
@@ -15,6 +15,10 @@ def rsvps_left()
   RSVP_LIMIT - rsvps.length
 end
 
+def rsvp(user)
+  REDIS.set "#{NEXT_EVENT}:#{user['email']}", user.to_jso
+end
+
 def already_rsvpd(email)
   REDIS.exists "#{NEXT_EVENT}:#{email}"
 end
@@ -38,7 +42,7 @@ def send_email(email,string)
   ses.send_email(
     :to => email,
     :from => 'someemail',
-    :subject => "Thanks for confirming for our #{NEXT_EVENT} event!"
+    :subject => "Thanks for confirming for our #{NEXT_EVENT} event!",
     :body => body
   )
 end
@@ -70,10 +74,15 @@ post '/rsvp' do
   if rsvps_left > 0 
     if captcha_pass? 
       user = params[:user]
+      unless (user[:first_name] && user[:last_name])
+	@msg = "Hey, friend.  Please enter your first and last name in case we have name tags."
+	erb :msg
+      end
       email = user["email"]
-      user["cancel"] = rand(36**15).to_s(36)
+      user["id"] = rand(36**15).to_s(36)
       if valid_email?(email)
 	unless already_rsvpd(email)
+	  rsvp(user)
 	  erb :confirmed
 	else
 	  @msg = "you are already rsvp'd for this event"
@@ -92,15 +101,21 @@ post '/rsvp' do
   end
 end
 
-get '/rsvp/cancel/:email/:authstring' do |email,authstring|
+get '/rsvp/cancel/:authstring' do |authstring|
+  @authstring = authstring
+  erb :confirm_cancel
+end
+
+post '/rsvp/cancel/:authstring' do |authstring|
+  email = params[:email]
   if already_rsvpd(email)
     if authstring == get_auth(email)
-      @msg "You have canceled from our #{NEXT_EVENT} event"
+      @msg = "You have canceled from our #{NEXT_EVENT} event"
     else
-      @msg "Sorry, I think this request is bogus.  Email us to cancel"
+      @msg = "Sorry, I think this request is bogus.  Email us to cancel"
     end
   else
-    @msg "Sorry, I do not know this email."
+    @msg = "Sorry, I do not know this email.  Email us from the account you registered from to cancel"
   end
   erb :msg
 end
